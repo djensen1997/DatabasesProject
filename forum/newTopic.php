3407d2e603824e46e7fed4e55cbb655811b7f8d1
ejmoore <ejmoore@mtu.edu>
2016-12-06 19:48:37 -0500

    refer to last commit

--- forum/newTopic.php
+++ forum/newTopic.php
@@ -5,7 +5,7 @@ try {
 	$dbh = new PDO('mysql:host=classdb.it.mtu.edu;dbname=airline', "cs3425", "grader");
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	$statement = $dbh->prepare("insert into topics values(".$title.",0,".$username.");"
+	$statement = $dbh->prepare("insert into topics values(".$title.",".$username.",0);"
 	$result = $statement->execute();
 } catch (PDOException $e) {
 	print "Error!" . $e->getMessage()."<br/>";
