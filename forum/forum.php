1b9e22554b9c8c19554ae113fa24a2efe275e507
DaneJensen <firstlegoboy@comcast.net>
2016-12-06 19:37:42 -0500

    fixed a concat glitch

--- forum/forum.php
+++ forum/forum.php
@@ -7,7 +7,7 @@
 	<?php
 		echo '<form action="newPost.php" method="post">';
 			echo '<input type="text" name="title" maxlength="100">';
-			echo '<input type="hidden" name="username" value=.$_GET['username']>';
+			echo '<input type="hidden" name="username" value='.$_GET['username'].'>';
 			echo '<input type="submit" name="subs" value="Add New Topic">';
 			echo '<br/>';
 		echo '</form>';
