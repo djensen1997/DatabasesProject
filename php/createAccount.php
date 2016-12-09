2b5e0f8aefd3a2743e7fbebc2ef669adcd85e000
DaneJensen <firstlegoboy@comcast.net>
2016-12-08 21:07:46 -0500

    stuff

--- php/createAccount.php
+++ php/createAccount.php
@@ -1,12 +1,12 @@
 <?php
 #handles adding new posts to the forum
 		try{
-			$dbh = new PDO('mysql:host=classdb.it.mtu.edu;dbname=danej', "cs3425gr", "cs3425gr");
+			$dbh = new PDO('mysql:host=classdb.it.mtu.edu;dbname=ejmoore', "cs3425gr", "cs3425gr");
 			$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			#call the add post procedure
 			if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['confirm'])){
 				if(strcmp($_POST['password'], $_POST['confirm']) == 0){
-					$dbh->query('insert into User values("'.$_POST['username'].'", "'.$_POST['password'].'")');
+					$dbh->query('insert into Student values("'.$_POST['sid'].'", "'.$_POST['password'].'")');
 				}else{
 					header('Location: newUser.html?msg='.urldecode("Passwords do not match"));
 				}
