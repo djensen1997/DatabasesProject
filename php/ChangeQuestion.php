027e279f2cc0b6cce16a3bc662ca8475f3b15366
DaneJensen <firstlegoboy@comcast.net>
2016-12-10 17:50:49 -0500

    another cap Q

--- php/ChangeQuestion.php
+++ php/ChangeQuestion.php
@@ -6,7 +6,7 @@
 	try{
 		$dbh = new PDO('mysql:host=classdb.it.mtu.edu;dbname=ejmoore', "cs3425gr", "cs3425gr");
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$question = $dbh->query("select * from question where eName='".$_POST['exam']."' and number=".$_POST['number']);
+		$question = $dbh->query("select * from Question where eName='".$_POST['exam']."' and number=".$_POST['number']);
 		foreach($question as $row){
 			if(strcmp($row[0], $_POST['qname']) != 0){
 				$dbh->query("update Question set question='".$_POST['qname']."' where eName='".$exam."' and number=".$num);
