e0926d2c757475786c5609c648e19e09416c2439
ejmoore <ejmoore@mtu.edu>
2016-12-10 17:10:48 -0500

    added exam grading

--- php/gradeQuestion.php
+++ php/gradeQuestion.php
@@ -8,19 +8,23 @@ try {
 	$eName = $_POST['eName'];
 	$number = $_POST['number'];
 	$answer = $_POST['choice'];
+	$points = $_POST['points'];
 	$correct = $_POST['correctAnswer'];
 	$dbh = new PDO('mysql:host=classdb.it.mtu.edu;dbname=ejmoore',"cs3425gr", "cs3425gr");
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	if (strcmp($answer,$correct) == 0) {
-		$dbh->query('insert into qGrade values('.$sId.',"'.$eName.'",'.$number.',1)');
+		$dbh->query('insert into qGrade values('.$sId.',"'.$eName.'",'.$number.',1,'.$points.')');
 	} else {
-		$dbh->query('insert into qGrade values('.$sId.',"'.$eName.'",'.$number.',0)');
+		$dbh->query('insert into qGrade values('.$sId.',"'.$eName.'",'.$number.',0)'.$points.')');
 	}
 
 	$maxNumber = mysql_fetch_array($dbh->query('select max(number) from Question where eName = "'.$eName.'"'));
 
 	if (strcmp($maxNumber[0],$number) == 0) {
+		$totalScore = mysql_fetch_array($dbh->query('select sum(points) from qGrade where eName="'.$eName.'" and correct=1'))[0];
+		$totalPoint = mysql_fetch_array($dbh->query('select sum(points) from qGrade where eName="'.$eName.'"'))[0];
+		$dbh->query('insert into eGrade values("'.$eName.'",'.$totalPoint.','.$totalScore.','.$sId.')');
 		echo '<form method="POST" id="form" action="../php/userportal.php">';
 	} else {
 		echo '<form method="POST" id="form" action="../php/question.php">';
