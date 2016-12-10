6bdfeac6e26cb633fce56eb5a608cff107accbcd
DaneJensen <firstlegoboy@comcast.net>
2016-12-10 18:08:22 -0500

    granted the ability to add questions

--- php/edit.php
+++ php/edit.php
@@ -8,6 +8,7 @@
 	$username = $_COOKIE['user'];
 	$exam = $_POST['exam'];
 	$questions;
+	$points;
 	try{
 		#get the exam names
 		$dbh = new PDO('mysql:host=classdb.it.mtu.edu;dbname=ejmoore', "cs3425gr", "cs3425gr");
@@ -35,6 +36,7 @@
 			echo "<TH> Worth </TH>";//7
 			echo "</TR>";
 			foreach($questions as $row){
+				$points += row[7];
 				echo '<form action="../php/editquestion.php" method="post">';
 				echo "<TR>";
 				echo "<TD>".$row[5]."</TD>";
@@ -50,6 +52,11 @@
 				echo "</TR>";
 				echo "<input type='hidden' name='exam' value='".$exam."'>";
 				echo "<input type='hidden' name='number' value='".$row[5]."'>";
+				echo '</form>';
+				echo '<form action="AddQuestion.php" method="post">';
+				echo '<input type="hidden" name="exam_name" value="'.$exam.'" >';
+				echo '<input type="hidden" name="points" value="'.$points.'" >';
+				echo '<input type="submit" name="addquestion" value="Add New Question">';
 				echo '</form>';
 			}
 			echo '</table>';
