2b8c21f9de4f8433ea5211301a4390d3634886d1
DaneJensen <firstlegoboy@comcast.net>
2016-12-10 17:47:34 -0500

    added a selected option

--- php/editquestion.php
+++ php/editquestion.php
@@ -1,7 +1,11 @@
 <?php 
 
 	try{
-
+		if(strcmp($corret,'A') == 0){
+			echo 'checked="checked">';
+		}else{
+			echo '>';
+		}
 		#get the exam names
 		$dbh = new PDO('mysql:host=classdb.it.mtu.edu;dbname=ejmoore', "cs3425gr", "cs3425gr");
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -37,22 +41,46 @@
 			echo "<TR>";
 			echo "<TD>Answer 1: </TD>";
 			echo "<TD><input type='text' name='a1' maxlength='255' value='".$choicea."'></TD>";
-			echo "<TD><input type='radio' name='correct' value='A' /></TD>";
+			echo "<TD><input type='radio' name='correct' value='A' "
+			if(strcmp($corret,'A') == 0){
+				echo 'checked="checked">';
+			}else{
+				echo '>';
+			}
+			echo "</TD>";
 			echo "</TR>";
 			echo "<TR>";
 			echo "<TD>Answer 2: </TD>";
 			echo "<TD><input type='text' name='a2' maxlength='255' value='".$choiceb."'></TD>";
-			echo "<TD><input type='radio' name='correct' value='B' /></TD>";
+			echo "<TD><input type='radio' name='correct' value='B'";
+			if(strcmp($corret,'B') == 0){
+				echo 'checked="checked">';
+			}else{
+				echo '/>';
+			}
+			echo  "</TD>";
 			echo "</TR>";
 			echo "<TR>";
 			echo "<TD>Answer 3: </TD>";
 			echo "<TD><input type='text' name='a3' maxlength='255' value='".$choicec."'></TD>";
-			echo "<TD><input type='radio' name='correct' value='C' /></TD>";
+			echo "<TD><input type='radio' name='correct' value='C'";
+			if(strcmp($corret,'C') == 0){
+				echo 'checked="checked">';
+			}else{
+				echo '/>';
+			}
+			echo  "</TD>";
 			echo "</TR>";
 			echo "<TR>";
 			echo "<TD>Answer 4: </TD>";
 			echo "<TD><input type='text' name='a4' maxlength='255' value='".$choiced."'></TD>";
-			echo "<TD><input type='radio' name='correct' value='D' /></TD>";
+			echo "<TD><input type='radio' name='correct' value='D'";
+			if(strcmp($corret,'D') == 0){
+				echo 'checked="checked">';
+			}else{
+				echo '/>';
+			}
+			echo  "</TD>";
 			echo "</TR>";
 			echo "<TR>";
 			echo "<TD>Points: <input type='text' name='points' value='".$point."''></TD>";
