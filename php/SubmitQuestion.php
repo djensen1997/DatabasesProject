33792b26dff30b6ffa545644864dd912b43a89b8
DaneJensen <firstlegoboy@comcast.net>
2016-12-12 13:58:20 -0500

    redid submit questinon

--- php/SubmitQuestion.php
+++ php/SubmitQuestion.php
@@ -6,14 +6,22 @@
 	try{
 		$dbh = new PDO('mysql:host=classdb.it.mtu.edu;dbname=ejmoore', "cs3425gr", "cs3425gr");
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$dbh->query("insert into Question values('".$_POST['qname']."', '".$_POST['a1']."', '".$_POST['a2']
-			."', '".$_POST['a3']."', '".$_POST['a4']."', ".$num.", '".$_POST['correct']."', ".$_POST['points'].", '".$exam."')");
+		//Question(qnum, ename, qname, points, correctChoice)
+		$dbh->query("insert into Question values( ".$num.", '".$exam."', '".$_POST['qname']."', ".
+			$_POST['points'].", '".$_POST['correct']."')");
+		$i = 0;
+		$char = 'A';
+		while(isset($_POST['a'.i])){
+			//Answer(qnum, ename, choice, value)
+			$dbh->query("insert into Answer values(".$num.", '".$exam."', '".$char++."', '".$_POST['a'.i]."'')");
+		}
 		$dbh->query("update Exam set points = points + ".$_POST['points']." where name = '".$exam."'");
 	}catch (PDOException $e){
 		print "ERROR!" . $e->getMessage()."<br/>";
 		die();
 	}
 	$num += 1;
+
 	setcookie('num', $num,  time() + (86400/48), "/");
 	if(isset($_POST['Finish'])){
 		unset($_COOKIE['exam']);
