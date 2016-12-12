786fd37a8d64035890b35574f7e4da83829fa2fd
DaneJensen <firstlegoboy@comcast.net>
2016-12-12 13:36:55 -0500

    made some letters

--- php/AddQuestions.php
+++ php/AddQuestions.php
@@ -55,19 +55,14 @@ if(isset($_POST['points'])){
 	echo "<TD>Select the Correct Answer</TD>";
 	echo "</TR>";
 	echo "<TR>";
-	echo "<TD>Answer 1: </TD>";
-	echo "<TD><input type='text' name='a1' maxlength='255'></TD>";
+	echo "<TD>Answer A: </TD>";
+	echo "<TD><input type='text' name='a1' maxlength='20'></TD>";
 	echo "<TD><input type='radio' name='correct' value='A' /></TD>";
 	echo "</TR>";
 	echo "<TR>";
-	echo "<TD>Answer 2: </TD>";
-	echo "<TD><input type='text' name='a2' maxlength='255'></TD>";
+	echo "<TD>Answer B: </TD>";
+	echo "<TD><input type='text' name='a2' maxlength='20'></TD>";
 	echo "<TD><input type='radio' name='correct' value='B' /></TD>";
-	echo "</TR>";
-	echo "<TR>";
-	echo "<TD>Answer 3: </TD>";
-	echo "<TD><input type='text' name='a3' maxlength='255'></TD>";
-	echo "<TD><input type='radio' name='correct' value='C' /></TD>";
 	echo "</TR>";
 	echo "<TR>";
 	echo '<TD></TD><TD><button type="button" onclick="addRow()">Add Another Choice</button></TD>';
@@ -96,8 +91,8 @@ if(isset($_POST['points'])){
 		var cell1 = row.insertCell(col++);
 		var cell2 = row.insertCell(col++);
 		var cell3 = row.insertCell(col++);
-		cell1.innerHTML = "Answer " + rowN + ":";
-		cell2.innerHTML = "<input type='text' name = 'a" + rowN + "' maxlength='20'>";
+		cell1.innerHTML = "Answer " + letter + ":";
+		cell2.innerHTML = "<input type='text' name = 'a" + (rowN-2) + "' maxlength='20'>";
 		cell3.innerHTML = "<input type='radio' name='correct' value='"+ letter + "' />";
 	}
 </script>
